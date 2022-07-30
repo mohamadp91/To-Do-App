3e08b34b4c55560d8840fd0e4a6c1192633498af
mohamadp91 <mohamad7291m@gmail.com>
2022-07-30 17:34:42 +0430

    config menus

--- app/src/main/java/com/example/to_do_app/fragments/add/AddFragment.kt
+++ app/src/main/java/com/example/to_do_app/fragments/add/AddFragment.kt
@@ -1,20 +1,34 @@
 package com.example.to_do_app.fragments.add
 
 import android.os.Bundle
+import android.view.*
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.lifecycle.Lifecycle
 import com.example.to_do_app.R
 
-class AddFragment : Fragment() {
+class AddFragment : Fragment(), MenuProvider {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        val view = inflater.inflate(R.layout.fragment_add, container, false)
+
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
+        return view
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.add_menu, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return false
     }
 
 }
